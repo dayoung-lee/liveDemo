5ca02499b45f08e2dd72f1cd62eb6b9a8a775f17
dayoung-lee <dayoung5213@gmail.com>
2020-01-19 23:16:09 -0500

    chage files

--- p2/js/clock.js
+++ p2/js/clock.js
@@ -4,17 +4,18 @@ const clockContainer = document.querySelector(".js-clock"),
 
 function getTime(){
     const date = new Date();
-    const hours = date.getHours();
+    var hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();    
-    const amPm = 'AM';
+    var amPm = 'AM';
     const currentDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-"
         + date.getDate();
 
     if(hours >= 12){
         amPm = 'PM';
-        hours = hors - 12;
+        hours = hours - 12;
     }
+
     clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
         minutes < 10 ? `0${minutes}` : minutes}:${
             seconds < 10 ? `0${seconds}` : seconds} ${amPm}`;
