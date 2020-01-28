0a1ce6d038d25f6fb877ccd4c1e207f41ae33e53
Dayoung Lee <55811738+dayoung-lee@users.noreply.github.com>
2020-01-28 14:20:46 -0500

    initial commit

---
+++ p6/build/precache-manifest.83643f1556683e34dd24a0f26829dc27.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108f0bdf656c459421e90ef9ba4d3bbd",
+    "url": "/restaurant-app/index.html"
+  },
+  {
+    "revision": "a2454caa3399b92470a6",
+    "url": "/restaurant-app/static/css/main.f54b7e47.chunk.css"
+  },
+  {
+    "revision": "aa5e70a4dc03056f8a4d",
+    "url": "/restaurant-app/static/js/2.4f7c5527.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/restaurant-app/static/js/2.4f7c5527.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2454caa3399b92470a6",
+    "url": "/restaurant-app/static/js/main.5d0fabdc.chunk.js"
+  },
+  {
+    "revision": "f68840457374e61ab4a2",
+    "url": "/restaurant-app/static/js/runtime-main.ab8f4000.js"
+  },
+  {
+    "revision": "4d9a5a88fd38d09e3ac477d0f21cdb06",
+    "url": "/restaurant-app/static/media/aboutImg1.4d9a5a88.jpg"
+  },
+  {
+    "revision": "3ffcb51767073b886764d659ee399a41",
+    "url": "/restaurant-app/static/media/appetizer.3ffcb517.png"
+  },
+  {
+    "revision": "25be68f7db515bfcf2bf56e207e56fa6",
+    "url": "/restaurant-app/static/media/main_noodle1.25be68f7.jpg"
+  },
+  {
+    "revision": "75d7b1d8e8d2cdc94b18748bbf9a47eb",
+    "url": "/restaurant-app/static/media/menu_img.75d7b1d8.jpg"
+  },
+  {
+    "revision": "3a6e367060e8c4ba02a1d5f0d34b0007",
+    "url": "/restaurant-app/static/media/review.3a6e3670.jpg"
+  }
+]);
---
+++ p6/src/Root.js
@@ -0,0 +1,14 @@
+// src/Root.js
+import React from 'react';
+import {HashRouter} from 'react-router-dom';
+import App from './App';
+
+const Root = () => {
+    return (
+        <HashRouter basename={'/' || `${process.env.PUBLIC_URL}`}>
+            <App />
+        </HashRouter>
+    );
+};
+
+export default Root;
